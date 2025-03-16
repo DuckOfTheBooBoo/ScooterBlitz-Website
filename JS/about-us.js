7dbbcf1e726632a36a15acbeb807d63dd46f10d5
Arajdian Altaf <school.altaf@gmail.com>
2025-03-16 13:29:33 +0700

    Enhance navigation behavior and styling on About Us page

--- JS/about-us.js
+++ JS/about-us.js
@@ -8,6 +8,8 @@ $(document).ready(function() {
     const typingSpeed = 60;
     const pauseDuration = 800;
 
+    $('nav').toggleClass('defaultNavState')
+
     function type() {
         if (partIndex < textArray.length) {
             if (charIndex <= textArray[partIndex].length) {
@@ -25,4 +27,30 @@ $(document).ready(function() {
     }
 
     type();
+
+    let atTop = true;
+
+    function checkScroll() {
+        if ($(window).scrollTop() === 0) {
+            if (!atTop) { 
+                atTop = true;
+                $('nav').removeClass('scrolledNavState')
+                $('nav').addClass('defaultNavState')
+                $('nav a').addClass('text-coolWhite')
+                $('nav a').removeClass('text-darkNavy')
+                console.log("User is at the top of the page");
+            }
+        } else {
+            if (atTop) { 
+                atTop = false;
+                $('nav').removeClass('defaultNavState')
+                $('nav').addClass('scrolledNavState')
+                $('nav a').addClass('text-darkNavy')
+                $('nav a').removeClass('text-coolWhite')
+                console.log("User scrolled down");
+            }
+        }
+    }
+
+    $(window).on("scroll", checkScroll);
 });
